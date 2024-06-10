af7db8fd2ac676cdd1a04b3be26086a9ffa24763
alballer08 <al08zhang@gmail.com>
2024-06-10 00:53:20 -0400

    Update CaesarCipher.h

--- CaesarCipher.h
+++ CaesarCipher.h
@@ -1,24 +1,24 @@
-//header file
-//include guard
+// header file
+// include guard
 #pragma once
 
-//compiler
+// compiler
 #include <string>
 
-class CaesarCipher{
+class CaesarCipher {
 private:
-//attribute
-int shiftKey;
-//shift function that can only be accessed from this class
-std::string shift(const std::string& text, int shiftKey) const;
+  // attribute
+  int shiftKey;
+  // shift function that can only be accessed from this class
+  std::string shift(const std::string &text, int shiftKey) const;
 
 public:
-//constructor to initialize the key variable
-CaesarCipher(int key);
-//encrypt function
-std::string encrypt(const std::string& plaintext) const;
-//decrypt function
-std::string decrypt(const std::string& ciphertext) const;
+  // constructor to initialize the key variable
+  CaesarCipher(int key);
+  // encrypt function
+  std::string encrypt(const std::string &plaintext) const;
+  // decrypt function
+  std::string decrypt(const std::string &ciphertext) const;
 
-void set_crypt(int s);
+  void set_crypt(int s);
 };
